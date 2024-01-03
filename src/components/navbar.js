12b572ba16b1ebfd7be79eee89a3806248dc0c54
SharonCoffee <120109024@umail.ucc.ie>
2024-01-03 21:09:18 +0000

    Adding navbar.js, about.js, contact.js, home.js, navbar.css files

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+// link with navbar.css
+import './css/navbar.css';
+
+const Navbar = () => {
+  return (
+        <nav className="navbar">
+            <div className="navbar-brand">CarbonTrack</div>
+            <ul className="navbar-nav">
+                <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+                <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+                {/* Add more navigation links here as needed */}
+            </ul>
+        </nav>
+  );
+};
+
+export default Navbar;
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB3Ydb30ttl_HXiK3wgJz8N239pc_mQLZ4",
-    authDomain: "carbontrack-a99b7.firebaseapp.com",
-    projectId: "carbontrack-a99b7",
-    storageBucket: "carbontrack-a99b7.appspot.com",
-    messagingSenderId: "620675691389",
-    appId: "1:620675691389:web:87768412a984ee340f1214",
-    measurementId: "G-K61Y9GMLQT"
+  apiKey: 'AIzaSyB3Ydb30ttl_HXiK3wgJz8N239pc_mQLZ4',
+  authDomain: 'carbontrack-a99b7.firebaseapp.com',
+  projectId: 'carbontrack-a99b7',
+  storageBucket: 'carbontrack-a99b7.appspot.com',
+  messagingSenderId: '620675691389',
+  appId: '1:620675691389:web:87768412a984ee340f1214',
+  measurementId: 'G-K61Y9GMLQT'
 };
 
 // Initialize Firebase

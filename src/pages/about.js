e34da04bdd5a5eabc7ef7591dcf7e219306a5eeb
SharonCoffee <72208574+SharonHenning@users.noreply.github.com>
2024-01-04 11:47:56 +0000

    Adjust firebase workflow yml file and make minor adjustments to about.js, contact.js and home.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 const About = () => {
-<<<<<<< HEAD
   return <div><h1>About Page</h1></div>;
 };
-=======
-    return <div><h1>About Page</h1></div>;
-}
->>>>>>> 55efd85b8fa197485cdad928eae601112b3dcba3
 
 export default About;
